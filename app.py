f2ac70f244a1ce2dbbc60647a4dd78eb72abb899
Rikhil Siripurapu <siripurapu.rikhil@gmail.com>
2026-06-02 11:38:46 +0530

    fix: indentation error in app.py

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ def stats():
 
 
 if __name__ == "__main__":
-app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
